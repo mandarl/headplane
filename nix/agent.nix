7833e1afdca16180b6f06b41d36c093d4a2a6e40
Mandar Limaye <mandarl@users.noreply.github.com>
2026-09-07 21:01:05 -0500

    fix(nix): placeholder vendorHash to discover new value from CI

--- nix/agent.nix
+++ nix/agent.nix
@@ -3,7 +3,7 @@ buildGoModule {
   pname = "hp_agent";
   version = (builtins.fromJSON (builtins.readFile ../package.json)).version;
   src = ../.;
-  vendorHash = "sha256-oxZF0p+6Bsdw4OWsUfGed4hfFH6EFxpyFLZdvDkiirM=";
+  vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
   ldflags = ["-s" "-w"];
   env.CGO_ENABLED = 0;
 }
