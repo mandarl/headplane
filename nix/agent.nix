bfba586968c08fd165c5595912710075ab50deb2
Mandar Limaye <mandarl@users.noreply.github.com>
2026-09-07 21:08:53 -0500

    fix(nix): update Go vendorHash for yaml.v3 dependency

--- nix/agent.nix
+++ nix/agent.nix
@@ -3,7 +3,7 @@ buildGoModule {
   pname = "hp_agent";
   version = (builtins.fromJSON (builtins.readFile ../package.json)).version;
   src = ../.;
-  vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  vendorHash = "sha256-phMNjw9U7vdFpPF9e+olpD+UoEsJfy8ilGaprubNdNc=";
   ldflags = ["-s" "-w"];
   env.CGO_ENABLED = 0;
 }
--- nix/ssh-wasm.nix
+++ nix/ssh-wasm.nix
@@ -14,7 +14,7 @@ in
     version = (builtins.fromJSON (builtins.readFile ../package.json)).version;
     src = ../.;
     subPackages = ["cmd/hp_ssh"];
-    vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    vendorHash = "sha256-phMNjw9U7vdFpPF9e+olpD+UoEsJfy8ilGaprubNdNc=";
     env.CGO_ENABLED = 0;
 
     nativeBuildInputs = [go];
