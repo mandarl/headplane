d4d01cd6393da748788d3151de0fc8f26c4a2a96
Mandar Limaye <mandarl@users.noreply.github.com>
2026-09-07 21:56:17 -0500

    fix(nix): update Go vendorHash for modernc.org/sqlite dependency

--- nix/agent.nix
+++ nix/agent.nix
@@ -3,7 +3,7 @@ buildGoModule {
   pname = "hp_agent";
   version = (builtins.fromJSON (builtins.readFile ../package.json)).version;
   src = ../.;
-  vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # TODO(phase2): discover from CI
+  vendorHash = "sha256-6e6GtHV+wA5Arkmv+3YgsIWNiW40JM2W4cATL+Bk02s=";
   ldflags = ["-s" "-w"];
   env.CGO_ENABLED = 0;
 }
--- nix/ssh-wasm.nix
+++ nix/ssh-wasm.nix
@@ -14,7 +14,7 @@ in
     version = (builtins.fromJSON (builtins.readFile ../package.json)).version;
     src = ../.;
     subPackages = ["cmd/hp_ssh"];
-    vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # TODO(phase2): discover from CI
+    vendorHash = "sha256-6e6GtHV+wA5Arkmv+3YgsIWNiW40JM2W4cATL+Bk02s=";
     env.CGO_ENABLED = 0;
 
     nativeBuildInputs = [go];
