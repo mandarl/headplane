f9ba2f40ef2fcc5e018c77060af72891c3b3db27
Mandar Limaye <mandarl@users.noreply.github.com>
2026-09-08 07:23:10 -0500

    fix(nix): update Go vendorHash for go-oidc/oauth2 dependencies

--- nix/ssh-wasm.nix
+++ nix/ssh-wasm.nix
@@ -14,7 +14,7 @@ in
     version = (builtins.fromJSON (builtins.readFile ../package.json)).version;
     src = ../.;
     subPackages = ["cmd/hp_ssh"];
-    vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    vendorHash = "sha256-SVJid7ddbJ7Srwlsy02iPcclSXRbDUImILsWjzg8Mu8=";
     env.CGO_ENABLED = 0;
 
     nativeBuildInputs = [go];
